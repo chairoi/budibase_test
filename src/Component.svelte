f3f22100698c9224b759020272500ef91d091a38
신채원 <87516785+chairoi@users.noreply.github.com>
2023-11-21 01:23:11 +0900

    fix bugs

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,17 +1,16 @@
 <script>
   import { getContext } from "svelte"
+  export let text = "text"
   const { styleable } = getContext("sdk")
   const component = getContext("component")
-
   function sum() {
-    return 3 + 2;
+      return 3 + 2;
   }
-
+  
   const result = sum();
-
   export let text = result;
 </script>
 
 <div use:styleable={$component.styles}>
-  This is a new-custom component. 3 + 2 is: {text}.
+  This is a new-custom component. The result of 3 + 2 is: {text}.
 </div>
