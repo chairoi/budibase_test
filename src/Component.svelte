593d4fea8cf1eb0f697ea9a0c3dfcea7bad44908
신채원 <87516785+chairoi@users.noreply.github.com>
2023-11-20 22:45:00 +0900

    Update Component.svelte
    initialize variable text

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,7 +1,7 @@
 <script>
   import { getContext } from "svelte"
 
-  export let text
+  export let text = "text"
 
   const { styleable } = getContext("sdk")
   const component = getContext("component")
