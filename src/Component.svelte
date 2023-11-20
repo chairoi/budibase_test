102245f21cc613aa1aaaf4d0c172a414d6a77696
신채원 <87516785+chairoi@users.noreply.github.com>
2023-11-21 01:09:44 +0900

    Update Component.svelte

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,16 +1,24 @@
 <script>
   import { getContext } from "svelte"
-  export let text = "text"
   const { styleable } = getContext("sdk")
   const component = getContext("component")
-  function sum() {
-      return 1 + 2;
-  }
   
-  const result = sum();
-  export let text = result;
+  const _text;
+
+  const importObject = {
+      env: {
+          __memory_base: 0,	
+      }
+  };
+  
+  WebAssembly.instantiateStreaming(fetch("./side_module.wasm"), importObject).then(result => {
+      const value = result.instance.exports.Increment(17);
+      _text = value.toString();
+  });
+
+  export let text = _text;
 </script>
 
 <div use:styleable={$component.styles}>
-  This is a new-custom component. The text setting is: {text}.
+  This is a new-custom component. 17 + 1 is: {text}.
 </div>
